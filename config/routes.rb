2a6df6e479f63af74766e3b10fceb64678bdb5f7
XueYing Chou <x.chou@gmx.de>
2021-08-17 15:31:32 +0200

    change how the routes were written

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pens#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get '/pens/:id', to: 'pens#index'
-get '/pens/:id', to: 'pens#show'
-end
+
+  resources :pens, only: [:show]
+  end
