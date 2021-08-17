8a15999e6b22c9e023eddf43287b8a99b5fee799
XueYing Chou <x.chou@gmx.de>
2021-08-17 17:09:06 +0200

    show-pens

--- app/controllers/pens_controller.rb
+++ app/controllers/pens_controller.rb
@@ -1,24 +1,38 @@
 class PensController < ApplicationController
 
   def index
-    # @pens = Pen.all
+    @pens = Pen.all
   end
 
-  # def show
-  #   @pens = Pen.new
-  #   @pens = Pen.find(param [:id])
-  # end 
+  def show
+    @pens = Pen.new
+    @pens = Pen.find(param [:id])
+  end 
 
-  # def create
-  # end
+  def create
+   @pens = Pen.all
+   @pens = Pen.new(pen_params)
+   if @pen.save
+    redirect_to pen_path(@pen)
+   else
+    render :new
+   end
+#end
 
-  # def edit
-  # end 
+  def edit
+    @pens.
+  end 
 
-  # def update
-  # end 
+  def update
+    if pen.update (pen_params)
+      redirect_to pen_path(@pen)
+   end 
 
-  # def destroy
-  # end
+  def destroy
+    if @pens.destroy
+      redirect_to index
+  end
 
 end
+
+private 
