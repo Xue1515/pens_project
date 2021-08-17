842ca60bf7eb14768ea44f2dab8da5fd16ad3bd0
Andreas Dahrendorf <ad@blinktank.de>
2021-08-17 16:16:43 +0200

    xxx

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_094041) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_bookings_on_item_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "pens", force: :cascade do |t|
     t.string "title"
