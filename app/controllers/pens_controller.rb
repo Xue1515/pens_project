31c0c29582da73119375e5ef5926398884065b40
XueYing Chou <x.chou@gmx.de>
2021-08-17 15:54:30 +0200

    linked homepage

--- app/controllers/pens_controller.rb
+++ app/controllers/pens_controller.rb
@@ -1,24 +1,24 @@
 class PensController < ApplicationController
-  
+
   def index
-    @pens = Pen.all
+    # @pens = Pen.all
   end
 
-  def show
-    @pens = Pen.new
-    @pens = Pen.find(param [:id])
-  end 
+  # def show
+  #   @pens = Pen.new
+  #   @pens = Pen.find(param [:id])
+  # end 
 
-  def create
-  end
+  # def create
+  # end
 
-  def edit
-  end 
+  # def edit
+  # end 
 
-  def update
-  end 
+  # def update
+  # end 
 
-  def destroy
-  end
+  # def destroy
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: "pens#index"
+    root to: "pens#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :pens, only: [:show]
-  end
+  resources :pens, only: [:show, :index]
+end
