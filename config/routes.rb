0ebe76ec0798e636bee717017ec457b1c466857f
XueYing Chou <x.chou@gmx.de>
2021-08-20 16:51:53 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pens#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pens do 
+  resources :pens, except: :index do
     resources :bookings, only: [ :new, :create]
   end
 end
